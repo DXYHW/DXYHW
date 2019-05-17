30e6ed59dd44517fb7e5b8beb90f3fa2c6d46a27
DXYHW <2637438356@qq.com>
2019-05-17 08:47:55 +0800

    第七天日考

---
+++ day7/Oyy.php
@@ -0,0 +1,27 @@
+<?php
+   class Oyy{
+       public function GetUglyNumber_Solution($index){
+           while($index == 2){
+               return $index;
+           }
+           while ($index ==3){
+               return $index;
+           }
+           while($index ==5){
+               return $index;
+           }
+           if ($index){
+               return true;
+           }else{
+               return false;
+           }
+       }
+       public function day7(){
+           $n = 1;
+           $num = 2;
+           $max = 1;
+           for ($i =1;$i<=$max;$i++ ){
+
+           }
+       }
+   }

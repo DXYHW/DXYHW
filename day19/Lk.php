a91b9da92305c619ba3144af50a5c6a7536d879c
DXYHW <2637438356@qq.com>
2019-05-31 08:50:34 +0800

    第十九天日考

---
+++ day19/Lk.php
@@ -0,0 +1,5 @@
+<?php
+ include  'Ui.php';
+ $res = new Ui();
+ $data = $res->dg(1234,3);
+ print_r($data);
---
+++ day19/Ui.php
@@ -0,0 +1,16 @@
+<?php
+ class Ui{
+     /*public function feidg($arr,$t){
+         foreach ($arr as $k=>$v){
+             if ($k==$t){
+                 return $arr
+             }
+         }
+     }*/
+     public function dg($arr,$t){
+         $i=0;
+         if ($t==$arr[i]){
+             return $this->dg($arr[i])
+         }
+     }
+ }

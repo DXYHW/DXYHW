9eb61fbff0a3a0bd3d34f24b5712ab5a4a13e1cc
DXYHW <2637438356@qq.com>
2019-06-03 08:45:53 +0800

    20

---
+++ day20/Ji.php
@@ -0,0 +1,11 @@
+<?php
+  class Ji{
+      public function CountSteps($x,$y){
+          for ($i=1;$i<=$x;$i++){
+              for ($j=1;$j<=$y;$i++){
+                  $sum = $x+$y;
+              }
+          }
+          return $sum;
+      }
+  }
---
+++ day20/Mi.php
@@ -0,0 +1,5 @@
+<?php
+  include 'Ji.php';
+  $res = new Ji();
+  $tt = $res->CountSteps(1,1);
+  print_r($tt);
